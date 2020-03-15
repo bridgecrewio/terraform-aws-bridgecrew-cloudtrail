673b5b9d8c5169129fec55e1220d01fc510cc453
Nimrod Kor <nimrodkor@gmail.com>
2020-03-16 00:32:08 +0200

    Move to prod

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   resource_name_prefix  = var.account_alias == "" ? "${var.company_name}-bc" : "${var.company_name}-${var.account_alias}"
-  bridgecrew_account_id = "090772183824"
-  bridgecrew_sns_topic  = "arn:aws:sns:${data.aws_region.region.name}:${local.bridgecrew_account_id}:handle-customer-actionsnimtest"
+  bridgecrew_account_id = "890234264427"
+  bridgecrew_sns_topic  = "arn:aws:sns:${data.aws_region.region.name}:${local.bridgecrew_account_id}:handle-customer-actions"
   log_file_prefix       = var.log_file_prefix == "" ? "" : "${var.log_file_prefix}/"
   profile_str           = var.aws_profile != "" ? "--profile ${var.aws_profile}" : ""
 
