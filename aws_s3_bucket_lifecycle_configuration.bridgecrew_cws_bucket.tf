b9971e30b05567d3504ff479e252276280182965
James Woolfenden <james.woolfenden@gmail.com>
2022-02-17 18:40:36 +0000

    fix attribute

--- aws_s3_bucket_lifecycle_configuration.bridgecrew_cws_bucket.tf
+++ aws_s3_bucket_lifecycle_configuration.bridgecrew_cws_bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_lifecycle_configuration" "bridgecrew_cws_bucket" {
   count  = var.existing_bucket_name == null ? 1 : 0
-  bucket = aws_s3_bucket.bridgecrew_cws_bucket[0].arn
+  bucket = aws_s3_bucket.bridgecrew_cws_bucket[0].bucket
   rule {
     id     = "Delete old log files"
     status = "Enabled"
